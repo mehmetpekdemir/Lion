401d26d30a760e0990fea77d67b62e87748d0537
Mehmet Pekdemir <mehmetpekdemir06@hotmail.com>
2020-07-31 01:17:58 +0300

    typos

---
+++ src/main/java/com/mehmetpekdemir/lion/dto/UserViewDTO.java
@@ -0,0 +1,33 @@
+package com.mehmetpekdemir.lion.dto;
+
+import java.io.Serializable;
+
+import com.mehmetpekdemir.lion.entity.User;
+
+import lombok.Getter;
+
+/**
+ * 
+ * @author MEHMET PEKDEMIR
+ * @since 1.0
+ */
+@Getter
+public class UserViewDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private final Long id;
+	private final String username;
+	private final String displayName;
+
+	private UserViewDTO(Long id, String username, String displayName) {
+		this.id = id;
+		this.username = username;
+		this.displayName = displayName;
+	}
+
+	public static UserViewDTO of(User user) {
+		return new UserViewDTO(user.getId(), user.getUsername(), user.getDisplayName());
+	}
+
+}
