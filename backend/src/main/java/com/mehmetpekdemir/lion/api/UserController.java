315f26a095a3241632e149c382898e70ac7d7dd6
Mehmet Pekdemir <mehmetpekdemir06@hotmail.com>
2020-08-14 02:28:08 +0300

    Added Pagination and getById.

--- backend/src/main/java/com/mehmetpekdemir/lion/api/UserController.java
+++ backend/src/main/java/com/mehmetpekdemir/lion/api/UserController.java
@@ -1,14 +1,20 @@
 package com.mehmetpekdemir.lion.api;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mehmetpekdemir.lion.dto.UserCreateDTO;
+import com.mehmetpekdemir.lion.dto.UserViewDTO;
 import com.mehmetpekdemir.lion.service.UserService;
 import com.mehmetpekdemir.lion.util.GenericResponse;
 
@@ -26,9 +32,23 @@ public class UserController {
 
 	private final UserService userService;
 
-	@PostMapping("v1/users")
+	// More Performance
+	@GetMapping("v1/users/slice")
+	public ResponseEntity<List<UserViewDTO>> sliceForUsers(Pageable pageable) {
+		final List<UserViewDTO> users = userService.sliceForUsers(pageable);
+		return ResponseEntity.ok(users);
+	}
+
+	@GetMapping("v1/user/{id}")
+	public ResponseEntity<UserViewDTO> getUserById(@PathVariable Long id) {
+		final UserViewDTO user = userService.getUserById(id);
+		return ResponseEntity.ok(user);
+	}
+
+	@PostMapping("v1/user")
 	public ResponseEntity<?> createUser(@Valid @RequestBody UserCreateDTO userCreateDTO) {
 		userService.createUser(userCreateDTO);
 		return ResponseEntity.ok(new GenericResponse("User Created"));
 	}
+
 }
--- backend/src/main/java/com/mehmetpekdemir/lion/service/impl/UserServiceImpl.java
+++ backend/src/main/java/com/mehmetpekdemir/lion/service/impl/UserServiceImpl.java
@@ -1,11 +1,16 @@
 package com.mehmetpekdemir.lion.service.impl;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.mehmetpekdemir.lion.dto.UserCreateDTO;
 import com.mehmetpekdemir.lion.dto.UserViewDTO;
 import com.mehmetpekdemir.lion.entity.User;
+import com.mehmetpekdemir.lion.error.NotFoundException;
 import com.mehmetpekdemir.lion.repository.UserRepository;
 import com.mehmetpekdemir.lion.service.UserService;
 
@@ -25,10 +30,21 @@ public class UserServiceImpl implements UserService {
 	private final PasswordEncoder passwordEncoder;
 
 	@Override
+	public List<UserViewDTO> sliceForUsers(Pageable pageable) {
+		return userRepository.findAll(pageable).stream().map(UserViewDTO::of).collect(Collectors.toList());
+	}
+
+	@Override
+	public UserViewDTO getUserById(Long id) {
+		final User user = userRepository.findById(id)
+				.orElseThrow(() -> new NotFoundException(String.format("User not found with id %d", id)));
+		return UserViewDTO.of(user);
+	}
+
+	@Override
 	public UserViewDTO createUser(UserCreateDTO userCreateDTO) {
 		encodeThePassword(userCreateDTO); // Password encoded
-
-		User user = userRepository.save(
+		final User user = userRepository.save(
 				new User(userCreateDTO.getUsername(), userCreateDTO.getDisplayName(), userCreateDTO.getPassword()));
 
 		return UserViewDTO.of(user);
@@ -37,4 +53,5 @@ public class UserServiceImpl implements UserService {
 	private void encodeThePassword(UserCreateDTO userCreateDTO) {
 		userCreateDTO.setPassword(this.passwordEncoder.encode(userCreateDTO.getPassword()));
 	}
+
 }
